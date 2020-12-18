a19e9718c8925b1e01b1af13e9dba76e5de4f7e2
abby <bansodeabhijit1997@gmail.com>
2020-12-18 12:53:06 +0530

    responsive fixes header

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import faker from "faker";
 
 const MainCard = styled("div")`
@@ -17,21 +17,20 @@ const CardHeader = styled("div")`
   background: #e8f5f9;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   border-top-right-radius: 5px;
   border-top-left-radius: 5px;
-  .chatboxUserCol{
-    display:flex;
-    flex-direction:column;
-    text-align:center;
-    height:7.5rem;
-    p{
-      color:black;
-      position:relative;
-      top:-20px;
-      margin:0px;
-      
+  .chatboxUserCol {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 7.5rem;
+    p {
+      color: black;
+      position: relative;
+      top: -20px;
+      margin: 0px;
     }
-   
   }
 `;
 
@@ -54,7 +53,7 @@ const InputContainer = styled("div")`
 const Chatbox = () => {
   return (
     <MainCard className="card">
-      <CardHeader>
+      <CardHeader className="pr-2">
         <div className="chatboxUserCol">
           <img
             alt="avatar"
@@ -71,7 +70,36 @@ const Chatbox = () => {
             )}`}
           />
           <p className="font-weight-bold">{faker.name.firstName()}</p>
-          <p className="font-weight-light text-secondary" >@{faker.name.firstName()}</p>
+          <p className="font-weight-light text-secondary">
+            @{faker.name.firstName()}
+          </p>
+        </div>
+        <div style={{ width: "60%" }} className="my-auto">
+          <p style={{fontSize:"1.5rem"}}>{faker.lorem.words()}</p>
+        </div>
+        <div className="my-auto">
+          <Button
+            style={{
+              color: "white",
+              fontSize: "1.2rem",
+
+              minWidth:"max-content",
+              marginTop: "1rem",
+              marginLeft: "0",
+              backgroundColor: "#6b6565",
+            }}
+          >
+            <span style={{ color: "#ffbb00" }} class="pr-3 h4">
+              i
+            </span>
+            Contact Info
+          </Button>
+        </div>
+        <div className="my-auto">
+          <i
+            style={{ cursor: "pointer",fontSize:"1.4rem"}}
+            class="fas fa-ellipsis-v mr-1 pt-3"
+          ></i>
         </div>
       </CardHeader>
       <MessagesContainer>
